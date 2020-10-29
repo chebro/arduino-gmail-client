4a3bf87161677cebd5ec56f8e9e27d4350e8719a
Sravanth C <54331348+sravanth-chebrolu@users.noreply.github.com>
2020-10-29 18:34:52 +0530

    Refactored code + .env nv now serves env vars

--- gmailLEDControl.py
+++ gmailLEDControl.py
@@ -5,11 +5,11 @@ def switchLED(state):
 	BOARD.digital[LED_PIN].write(state)
 
 BOARD = Arduino('/dev/ttyACM0')
-ACCESS_TOKEN = json.load(open('./access_token.json', 'r'))['access_token']
+ACCESS_TOKEN = json.load(open('./bin/data/access_token.json', 'r'))['access_token']
 LED_PIN = 13
 
 while True:
-    response = int(os.popen(f'./getUnreadEmailCount.sh {ACCESS_TOKEN}').read())
-    print(response)
+    response = int(os.popen(f'./bin/getUnreadEmailCount.sh {ACCESS_TOKEN}').read())
+    print('Total Unread Emails:', response)
     switchLED(1) if response else switchLED(0)
     time.sleep(5)
